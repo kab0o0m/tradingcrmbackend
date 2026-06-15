b9b7aa8bb82da3855f480fbc6bc4f71f6ccbb28b
kab0o0m <jroot97@hotmail.com>
2026-06-15 17:32:23 +0800

    added cors for vercel app

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ app.include_router(news_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://tradingcrm.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
